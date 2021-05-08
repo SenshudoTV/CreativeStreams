87cf2a0689e348c9b852f2efe0140f5ac3d43292
Stewart Marsh <stewart.marsh@senshudo.tv>
2021-05-08 16:46:48 +0100

    chore: cleanup build script

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 APPLICATION_NAME=creativestreams
 
-ssh-add ~/.ssh/id_rsa
 echo "DOCKER_BUILDKIT=1 docker build -t $APPLICATION_NAME ."
 DOCKER_BUILDKIT=1 docker build -t "$APPLICATION_NAME" . --progress=plain
